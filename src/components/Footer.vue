14c06a6b11d362f940d6952986030dc794bfd5cb
ibrahim-mufti <ibrahimmufti36@gmail.com>
2023-09-25 13:25:42 +0500

    src files

---
+++ src/components/Footer.vue
@@ -0,0 +1,59 @@
+<template>
+    <footer class="py-32">
+        <div class="container mx-auto max-md:!max-w-75 max-sm:!max-w-30 max-lg:!max-w-90">
+            <div class="flex flex-row justify-evenly mb-32 max-md:flex-col max-lg:flex-col">
+                <div class="w-1/4 max-md:w-full max-md: mb-4">
+                    <h2 class="text-lg font mb-4">Tasty</h2>
+                    <p class="color">Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit. Eos cumque dicta adipisci architecto culpa amet.</p>
+                </div>
+                <div>
+                    <h2>Links</h2>
+                    <ul class="my-4">
+                        <li class="color"><a class="hover:text-red-600 transition-all duration-300 ease-linear" href="#">Home</a></li>
+                        <li class="color"><a class="hover:text-red-600 transition-all duration-300 ease-linear" href="#">About</a></li>
+                        <li class="color"><a class="hover:text-red-600 transition-all duration-300 ease-linear" href="#">Menu</a></li>
+                        <li class="color"><a class="hover:text-red-600 transition-all duration-300 ease-linear" href="#">Gallery</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h2>Categories</h2>
+                    <ul class="my-4">
+                        <li class="color" ><a class="hover:text-red-600 transition-all duration-300 ease-linear" href="#">Landing page</a></li>
+                        <li class="color" ><a class="hover:text-red-600 transition-all duration-300 ease-linear" href="#">Real Estate</a></li>
+                        <li class="color" ><a class="hover:text-red-600 transition-all duration-300 ease-linear" href="#">Personal</a></li>
+                        <li class="color" ><a class="hover:text-red-600 transition-all duration-300 ease-linear" href="#">Business</a></li>
+                        <li class="color" ><a class="hover:text-red-600 transition-all duration-300 ease-linear" href="#">E-commerce</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h2>Contact information</h2>
+                    <ul class="my-4 color">
+                        <li><p>198 West 21th Street,<br>Suite 721 New York NY 10016</p></li>
+                        <li><a class="hover:text-red-600 transition-all duration-300 ease-linear"  href="#">+ 1235 2355 98</a></li>
+                        <li><a class="hover:text-red-600 transition-all duration-300 ease-linear"  href="#">info@yoursite.com</a></li>
+                        <li><a class="hover:text-red-600 transition-all duration-300 ease-linear"  href="freehtml5.co">freehtml5.co</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="text-center mb-6">
+                <p class="color">&copy; 2016 Free HTML5. All Rights Reserved.<br> Designed by <a class="text-red-600" href="freehtml5.co">FreeHTML5.co </a>Demo Images: <a class="text-red-600" >Unsplash</a></p>
+            </div>
+            <div class="flex text-2xl text-red-600 flex-row justify-center text-center">
+                <p class="mr-3"><fa :icon="['fab',`twitter`]"></fa></p>
+                <p class="mr-3"><fa :icon="['fab',`facebook`]"></fa></p>
+                <p class="mr-3"><fa :icon="['fab',`linkedin`]"></fa></p>
+                <p class="mr-3"><fa :icon="['fab',`dribbble`]"></fa></p>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+    export default {
+        name : 'footer',
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Gallery_images.vue
@@ -0,0 +1,213 @@
+<template>
+    <section class="py-32">
+        <div class="container mx-auto text-left max-md:!max-w-75 max-sm:!max-w-30 max-lg:!max-w-90">
+            <div class="w-1/2 max-sm:w-full max-lg:w-full">
+                <h2 class="fnt text-4xl mb-8 ">Our Gallery</h2>
+                <p class="color">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis ab debitis sit itaque totam, a maiores nihil, nulla magnam porro minima officiis! Doloribus aliquam voluptates corporis et tempora consequuntur ipsam, itaque, nesciunt similique commodi omnis. Ad magni perspiciatis, voluptatum repellat.</p>
+            </div>
+        </div>
+    </section>
+    <section class="py-32">
+        <div class="container mx-auto text-center max-lg:ml-6 max-md:!max-w-75 max-sm:!max-w-30 max-lg:!max-w-90 after:zindex">
+            <div class="full_img" id="fullimgBox">
+                <img @click= "closeimg" id="fullimg"  src="../components/Images/gallery_8.jpeg">
+            </div>
+            <div class="grid grid-rows-4 gap-4">
+                <div class="img-gallery3">
+                    <div class="allignment3">
+                        <div>
+                            <img @click="fullimg " src="../components/Images/gallery_1.jpeg">
+                        </div>
+                        <div>
+                            <img @click="fullimg " src="../components/Images/gallery_2.jpeg">
+                        </div>
+                    </div>
+                    <div>
+                        <img @click="fullimg " src="../components/Images/gallery_8.jpeg">
+                    </div>
+                    <div class="allignment3">
+                        <div>
+                            <img @click="fullimg " src="../components/Images/gallery_7.jpeg" alt="">
+                        </div>
+                        <div>
+                            <img @click="fullimg " src="../components/Images/gallery_5.jpeg" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="img-gallery">
+                    <div class="allignment3">  
+                        <div>
+                            <img @click="fullimg " src="../components/Images/gallery_5.jpeg" alt="">
+                        </div>
+                        <div>
+                            <img @click="fullimg " src="../components/Images/gallery_6.jpeg" alt="">
+                        </div>
+                    </div>
+                    <div class="allignment3">
+                        <div>
+                            <img @click="fullimg " src="../components/Images/gallery_7.jpeg" alt="">
+                        </div>
+                        <div>
+                            <img @click="fullimg " src="../components/Images/gallery_8.jpeg" alt="">
+                        </div>
+                    </div>
+                    <div class="h-auto">
+                        <img @click="fullimg " src="../components/Images/gallery_9.jpeg" alt="">
+                    </div>
+                </div>
+                <div class="img-gallery3">
+                    <div class="allignment3">
+                        <div>
+                            <img @click="fullimg " src="../components/Images/gallery_1.jpeg">
+                        </div>
+                        <div>
+                            <img @click="fullimg " src="../components/Images/gallery_2.jpeg">
+                        </div>
+                    </div>
+                    <div>
+                        <img @click="fullimg " src="../components/Images/gallery_8.jpeg">
+                    </div>
+                    <div class="allignment3">
+                        <div>
+                            <img @click="fullimg " src="../components/Images/gallery_7.jpeg" alt="">
+                        </div>
+                        <div>
+                            <img @click="fullimg " src="../components/Images/gallery_5.jpeg" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="img-gallery">
+                    <div class="allignment3">  
+                        <div>
+                            <img @click="fullimg " src="../components/Images/gallery_5.jpeg" alt="">
+                        </div>
+                        <div>
+                            <img @click="fullimg " src="../components/Images/gallery_6.jpeg" alt="">
+                        </div>
+                    </div>
+                    <div class="allignment3">
+                        <div>
+                            <img @click="fullimg " src="../components/Images/gallery_7.jpeg" alt="">
+                        </div>
+                        <div>
+                            <img @click="fullimg " src="../components/Images/gallery_8.jpeg" alt="">
+                        </div>
+                    </div>
+                    <div class="h-auto">
+                        <img @click="fullimg " src="../components/Images/gallery_9.jpeg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name : "Gallery_images",
+        methods:{
+            fullimg(event){
+                let imgbox = document.getElementById('fullimgBox')
+                let fullimg = document.getElementById('fullimg')
+
+                imgbox.style.display = 'flex'
+                fullimg.src = event.srcElement.getAttribute("src")
+            },
+            closeimg(){
+                let imgbox = document.getElementById('fullimgBox')
+                let fullimg = document.getElementById('fullimg')
+                imgbox.style.display = "none"
+                fullimg.style.cursor = "zoom-out"
+                
+            }   
+        }
+    } 
+</script>
+
+<style scoped>
+    .zindex{
+        z-index: 150;
+    }
+    .img-gallery{
+        margin-left: 0;
+        margin-right: 0;
+        margin-top: auto;
+        margin-bottom:auto;
+        display: grid;
+        grid-template-columns:25% 25% 50%;
+        gap: 20px;
+    }
+    img {
+        width: 100%;
+        height: 100%;
+    }
+    .allignment{
+        width:100%;
+        display: grid;
+        grid-template-rows: 50% 50%;
+        gap: 10px;
+    }
+    .img-gallery3{
+        margin-left: 0;
+        margin-right: 0;
+        margin-top: auto;
+        margin-bottom:auto;
+        width:100%;
+        display: grid;
+        grid-template-columns: 25% 50% 25%;
+        gap:20px;
+    }
+    .allignment3{
+        width:100%;
+        display: grid;
+        grid-template-rows: 250px 250px;
+        gap: 20px;
+    }
+    .full_img{
+        width: 100%;
+        height: 100%;
+        background: rgba(0,0,0,0.9);
+        position: fixed;
+        top:0;
+        left: 0;
+        display: none;
+        align-items: center;
+        justify-content: center;
+    }
+    .full_img img{
+        width: 50%;
+        height:80%;
+    }
+    img:hover{
+        cursor: zoom-in;
+
+    }
+
+    @media (max-width:768px) {
+        .img-gallery, .img-gallery3{
+            grid-template-columns: 100%;
+        }
+        .allignment, .allignment3 {
+            grid-template-rows: 50% 50%;
+            height:98%
+        }
+        .full_img img{
+            width: 90%;
+            height: 80%;
+        }
+    }
+    @media (max-width:600px) {
+        .img-gallery, .img-gallery3{
+            grid-template-columns: 100%;
+            height: 100%;
+        }
+        .allignment, .allignment3 {
+            grid-template-rows: 50% 50%;
+            height:96%
+        }
+        .full_img img{
+            width: 90%;
+            height: 50%;
+        }
+    }
+</style>
