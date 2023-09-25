ef43b9d3dd370c9a7aa1ac9fd2eb19a33e5004ce
ibrahim-mufti <ibrahimmufti36@gmail.com>
2023-09-25 15:32:44 +0500

    src files

---
+++ src/components/CardSection.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="py-32">
+        <div class="container mx-auto text-center max-md:!max-w-75 max-sm:!max-w-30 max-lg:!max-w-90">
+            <div class="pb-32">
+                <h2 class="fnt text-3xl">The Event</h2>
+                <p class="text-xl color">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, consequatur. Aliquam quaerat pariatur repellendus veniam nemo, saepe, culpa eius aspernatur cumque suscipit quae nobis illo tempora. Eum veniam necessitatibus, blanditiis facilis quidem dolore! Dolorem, molestiae.</p>
+            </div>
+            <div class="flex flex-row justify-evenly text-left max-md:flex-col max-lg:flex-col">
+                <div class="width bgblack pb-5 max-md:mb-5 max-sm:mb-5 max-lg:mb-5">
+                        <img class="mb-28" src="./Images/gallery_1.jpeg" alt="">
+                    <div class="mr-4 ml-6">
+                        <h2 class="mb-5 text-3xl font-bold">Photoshoot of The Street</h2>
+                        <p class="mb-16 color">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>                        
+                        <ul class="flex flex-row">
+                            <li class="ml-3"><fa class="text-xs color mr-1" icon="heart"/>1.2k</li>
+                            <li class="ml-3"><fa class="text-xs color mr-1" icon="eye"/>1.2k</li>
+                            <div class="flex flex-row w-7/12 justify-end">
+                                <a href="/" class="font-thin text-red-600 hover:text-red-800 transition-all duration-300 ease-linear"><fa class="color" icon="arrow-right"></fa> Read More</a>
+                            </div>
+                        </ul>
+                    </div>     
+                </div>
+                <div class="width bgblack pb-5 max-md:mb-5 max-sm:mb-5 max-lg:mb-5">
+                    <img class="mb-28" src="./Images/gallery_2.jpeg" alt="">
+                    <div class="mr-4 ml-6">
+                        <h2 class="mb-5 text-3xl font-bold">Surfing At Phillippines</h2>
+                        <p class="mb-16 color">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>                        
+                        <ul class="flex flex-row">
+                            <li class="ml-3"><fa class="text-xs color mr-1" icon="heart"/>1.2k</li>
+                            <li class="ml-3"><fa class="text-xs color mr-1" icon="eye"/>1.2k</li>
+                            <div class="flex flex-row w-7/12 justify-end">
+                                <a href="/" class="font-thin text-red-600 hover:text-red-800 transition-all duration-300 ease-linear"><fa class="color" icon="arrow-right"></fa> Read More</a>
+                            </div>
+                        </ul>
+                    </div>
+                </div>
+                <div class="width bgblack pb-5 max-md:mb-5 max-sm:mb-5">
+                    <img class='mb-28' src="./Images/gallery_6.jpeg" alt="">
+                    <div class="mr-4 ml-6">
+                        <h2 class="mb-5 text-3xl font-bold">Focus on Underwater</h2>
+                        <p class="mb-16 color">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>                        
+                        <ul class="flex flex-row">
+                            <li class="ml-3"><fa class="text-xs color mr-1" icon="heart"/>1.2k</li>
+                            <li class="ml-3"><fa class="text-xs color mr-1" icon="eye"/>1.2k</li>
+                            <div class="flex flex-row w-7/12 justify-end">
+                                <a href="/" class="font-thin text-red-600 hover:text-red-800 transition-all duration-300 ease-linear"><fa class="color" icon="arrow-right"></fa> Read More</a>
+                            </div>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        name : 'event',
+    }
+</script>
+
+<style scoped>
+    .width {
+        width:30%;
+    }
+    .bgblack{
+        background-color: rgb(29, 29, 29);
+    }
+    @media (max-width:768px) {
+        .width{
+            width:100%
+        }
+    }
+    @media(max-width:1024px){
+        .width {
+            width: 100%;
+        }
+    }
+</style>
---
+++ src/components/ContactForm.vue
@@ -0,0 +1,56 @@
+<template>
+        <section class="py-32">
+            <div class="container mx-auto text-center max-md:!max-w-75 max-sm:!max-w-30 max-lg:!max-w-90">
+                <div>
+                    <h2 class="text-4xl fnt font-medium mb-8">Don't be shy, let's chat.</h2>
+                    <p class="w-2/3 mb-8 mx-auto text-xl color max-lg:w-full"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae enim quae vitae cupiditate, sequi quam ea id dolor reiciendis consectetur repudiandae. Rem quam, repellendus veniam ipsa fuga maxime odio? Eaque!</p>
+                    <a href="#" class="btn">Contact Us</a>
+                </div>
+            </div>
+        </section>
+        <section class="py-20">
+            <div class="container mx-auto left max-md:!max-w-75 max-sm:!max-w-30 max-lg:!max-w-90">
+                <div class="pt-32 flex flex-row justify-end ">
+                    <div class="flex flex-col w-1/2 BD-box py-10 px-10 max-md:w-full">
+                        <label class="text-xl uppercase font-bold mb-2" for="Name">Your Name</label>
+                        <input class="bg-transparent BD w-full h-12 mb-6 p-2" type="text">
+                        <label class="text-xl uppercase font-bold mb-2" for="Email">Your Email</label>
+                        <input class="bg-transparent BD w-full h-12 mb-6 p-2" type="text">
+                        <label class="text-xl uppercase font-bold mb-2" for="message">Your Message</label>
+                        <textarea class="bg-transparent BD w-full h-auto mb-10 p-2" cols="30" rows="10"></textarea>
+                        <div>
+                            <a href="#" class="btn">Submit Form</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+</template>
+
+<script>
+    export default {
+        name : "CF",
+    }
+</script>
+
+<style scoped>
+    .btn{
+        text-transform: uppercase;
+        border:2px solid red;
+        padding: 10px;
+        padding-left: 25px;
+        padding-right: 25px;
+    }
+    .btn:hover{
+        background-color: red;
+        transition: all .5s ease;
+    }
+    .BD-box{
+        border: 4px solid rgb(67, 66, 66);
+    }
+    .BD{
+        border: 2px solid rgb(67, 66, 66);
+        border-radius: 5px;
+    }
+
+</style>
